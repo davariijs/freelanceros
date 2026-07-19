76d05c30d48d0b5d4c7e6a01e4d07667ede5a8ad
davarijs <davari@delivando.com>
2026-07-19 18:46:21 +0330

    Fix Pino

--- apps/api/src/utils/logger.ts
+++ apps/api/src/utils/logger.ts
@@ -1,20 +1,18 @@
 import pino from 'pino';
 
 const isTest = process.env.NODE_ENV === 'test';
+const isProd = process.env.NODE_ENV === 'production';
 
 export const logger = pino({
-  level: isTest
-    ? 'silent'
-    : process.env.NODE_ENV === 'production'
-      ? 'info'
-      : 'debug',
-  transport: isTest
-    ? undefined
-    : {
-        target: 'pino-pretty',
-        options: {
-          colorize: true,
-          ignore: 'pid,hostname',
+  level: isTest ? 'silent' : isProd ? 'info' : 'debug',
+  transport:
+    isTest || isProd
+      ? undefined
+      : {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            ignore: 'pid,hostname',
+          },
         },
-      },
 });
